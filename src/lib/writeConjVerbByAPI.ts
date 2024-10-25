00b1004de2b7d78dfa348cf792ed5c280ff02501
renribsilva <ren.rib.silva@gmail.com>
2024-10-25 11:16:46 -0300

    arquivos modificados

--- src/lib/writeConjVerbByAPI.ts
+++ src/lib/writeConjVerbByAPI.ts
@@ -1,11 +1,12 @@
 import { conjugateVerb } from './conjugateVerb';
 
 export const writeConjVerbByAPI = async (verb: string) => {
-
   // Chama a função de conjugação
   const conjugations = conjugateVerb(verb);
 
   try {
+    console.log('Enviando verbo e conjugações para a API:', { verb, conjugations });
+
     const response = await fetch('/api/conjVerb', {
       method: 'POST',
       headers: {
@@ -15,12 +16,14 @@ export const writeConjVerbByAPI = async (verb: string) => {
     });
 
     if (!response.ok) {
+      const errorData = await response.json();
+      console.error('Erro na resposta da API:', errorData);
       throw new Error(`Erro ${response.status}: ao escrever o arquivo file.json em writeConjVerbByAPI`);
     }
 
     const data = await response.json();
-    console.log(data.message); // Exibe a mensagem de sucesso
+    console.log('Resposta da API:', data.message); // Exibe a mensagem de sucesso
   } catch (error) {
-    console.error('writeConjVerbByAPI failed:', error);
+    console.error('writeConjVerbByAPI failed:', error, 'verb:', verb, 'conjugations:', conjugations);
   }
 };
