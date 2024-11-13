f875ec6f606cba428ecac60180b74718e2b04db7
renribsilva <ren.rib.silva@gmail.com>
2024-11-13 14:46:55 -0300

    arquivos modificados

--- src/lib/isIrregVerbByAPI.ts
+++
@@ -1,9 +0,0 @@
-export async function isIrregVerbByAPI(verb: string) {
-  try {
-    const response = await fetch(`/api/isIrregVerb?verb=${verb}`);
-    if (!response.ok) throw new Error((await response.json()).error);
-    return (await response.json()).result;
-  } catch (error) {
-    throw new Error('isIrregVerbByAPI failed: ' + error.message);
-  }
-}
