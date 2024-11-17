1f04c4d06cb754e0d933a5f5a4cb800ea5a155a5
renribsilva <ren.rib.silva@gmail.com>
2024-11-17 12:14:11 -0300

    arquivos modificados

--- scripts/addNewVerbs.ts
+++ scripts/addNewVerbs.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import readTxtLines from './readTxtLines';
+import { nw } from '../src/lib/normalizeVerb';
 
 const assetsDir = path.join(process.cwd(), 'assets');
 
@@ -7,8 +8,9 @@ export default async function addNewVerbs(words: string[]): Promise<string[]> {
   try {
 
     const newWordPath = path.join(assetsDir, 'newVerbs.txt');
-    const additionalWords = await readTxtLines(newWordPath);
-    return [...words, ...additionalWords];
+    const newWords = await readTxtLines(newWordPath);
+    const normnewWords = newWords.map(verb => nw(verb));
+    return [...words, ...normnewWords];
 
   } catch (err) {
 
