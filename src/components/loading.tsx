58eea0f6c2e359043aeec00c00a46267fa6e1deb
renribsilva <ren.rib.silva@gmail.com>
2024-11-26 20:42:45 -0300

    arquivos modificados

---
+++ src/components/loading.tsx
@@ -0,0 +1,11 @@
+const LoadingDots = () => {
+  return (
+    <div className="loading-dots">
+      <span>.</span>
+      <span>.</span>
+      <span>.</span>
+    </div>
+  );
+};
+
+export default LoadingDots;
