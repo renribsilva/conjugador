33bfea52a1695829f4cd9d3d3ce927469b67ef26
renribsilva <ren.rib.silva@gmail.com>
2024-11-26 20:00:15 -0300

    arquivos modificados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,29 @@ import type { AppProps } from 'next/app';
 import React from 'react'; // Corrigido aqui
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
+import Head from 'next/head';
+
+const title = "Gules"
+const description = "Conjugador de verbos da Língua Porguesa Brasileira"
+const url = "https://conjugador-gules.vercel.app"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+
+        <title>{title}</title>
+
+        {/* Open Graph */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        {/* <meta property="og:image" content={image} /> */}
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="link" />
+        <meta property="og:site_name" content="Conjugador-Gules" />
+        <meta property="og:author_name" content="renribsilva" />
+        <meta property="og:provider_name" content="Gules" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
