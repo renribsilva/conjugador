02c19360aea1655c274953c3700db4c23f464705
renribsilva <ren.rib.silva@gmail.com>
2025-01-03 11:53:50 -0300

    arquivos modificados

--- scripts/editAllVerbJson.ts
+++ scripts/editAllVerbJson.ts
@@ -147,7 +147,7 @@ async function processVerbsFile(): Promise<void> {
             try {
               
               // const input = normalized
-              const input = "despescar"
+              const input = "desprogramar"
               let verbPropsArray = cache.get(input);
               if (!verbPropsArray) {
                 verbPropsArray = await getPropsOfVerb(input, true, input);
