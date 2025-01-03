c1a23b13a0b04fcc8ace2d319363ac17dabd2612
renribsilva <ren.rib.silva@gmail.com>
2025-01-03 14:30:52 -0300

    arquivos modificados

--- scripts/editAllVerbJson.ts
+++ scripts/editAllVerbJson.ts
@@ -147,7 +147,7 @@ async function processVerbsFile(): Promise<void> {
             try {
               
               // const input = normalized
-              const input = "desprogramar"
+              const input = "encarrapichar"
               let verbPropsArray = cache.get(input);
               if (!verbPropsArray) {
                 verbPropsArray = await getPropsOfVerb(input, true, input);
