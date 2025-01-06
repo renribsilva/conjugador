9a5ec474c41bf7a05f44f8b0d9488c2f3cba061d
renribsilva <ren.rib.silva@gmail.com>
2025-01-06 10:53:16 -0300

    arquivos modificados

--- scripts/editAllVerbJson.ts
+++ scripts/editAllVerbJson.ts
@@ -147,7 +147,7 @@ async function processVerbsFile(): Promise<void> {
             try {
               
               // const input = normalized
-              const input = "inutilizar"
+              const input = "literalizar"
               let verbPropsArray = cache.get(input);
               if (!verbPropsArray) {
                 verbPropsArray = await getPropsOfVerb(input, true, input);
