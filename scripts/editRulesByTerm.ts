669956ba077dd299f67730c99fd65de2792c0c8c
renribsilva <ren.rib.silva@gmail.com>
2025-01-16 18:17:25 -0300

    arquivos modificados

--- scripts/editRulesByTerm.ts
+++ scripts/editRulesByTerm.ts
@@ -71,7 +71,7 @@ async function addVerbsToJson() {
     let dataChanged = false;
     const startTime = Date.now();
 
-    let specificMainKey: string | string[] | null = ["ançar"]
+    let specificMainKey: string | string[] | null = ["einar"]
     if (Array.isArray(specificMainKey)) {
       specificMainKey = Array.from(new Set(specificMainKey));
     }
