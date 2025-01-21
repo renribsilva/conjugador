44284f21d0fd7e7c0292ce19c225d37cfc0f0605
renribsilva <ren.rib.silva@gmail.com>
2025-01-21 09:52:51 -0300

    arquivos modificados

--- scripts/editRulesByTermJson.ts
+++ scripts/editRulesByTermJson.ts
@@ -72,7 +72,7 @@ async function editRulesByTerm() {
     let dataChanged = false;
     const startTime = Date.now();
 
-    let specificMainKey: string | string[] | null = ["irgar"]
+    let specificMainKey: string | string[] | null = ["esver"]
     if (Array.isArray(specificMainKey)) {
       specificMainKey = Array.from(new Set(specificMainKey));
     }
