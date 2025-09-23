9b2828bab9735a2c761e6d6e07f1eed4fe54c937
renribsilva <ren.rib.silva@gmail.com>
2025-09-23 15:12:59 -0300

    arquivos modificados

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
       <span>© {currentYear} MIT</span>
       <span> | </span>
       <span>
-        criado por um humano com:{" "}
+        criado com:{" "}
         {linkfooter.map(({ label, href }) => (
           <Link key={href} href={href} {...linkProps} prefetch={false}>
           {label}
