cfef3830984a5e15a7931e334bf44ba35f9a15d4
renribsilva <ren.rib.silva@gmail.com>
2025-09-25 20:24:27 -0300

    arquivos modificados

--- src/components/svgs/arrow.tsx
+++ src/components/svgs/arrow.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Arrow = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" fill="currentColor" viewBox="0 -1000 960 960" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" height="21px" width="21px" fill="currentColor" viewBox="0 -1000 960 960" {...props}>
     <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"/>
   </svg>
 );
