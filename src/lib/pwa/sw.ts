546e50035a33527205c1636e970a66b57bc51c61
renribsilva <ren.rib.silva@gmail.com>
2025-09-29 12:25:39 -0300

    arquivos modificados

--- src/lib/pwa/sw.ts
+++ src/lib/pwa/sw.ts
@@ -48,3 +48,9 @@ function sendStatusToClients(status: boolean) {
     });
   });
 }
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
