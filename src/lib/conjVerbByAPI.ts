5ccb246f0ea3d9c2e55d2faf5bbe3a2c3247a445
renribsilva <ren.rib.silva@gmail.com>
2025-09-30 10:25:18 -0300

    arquivos modificados

--- src/lib/conjVerbByAPI.ts
+++ src/lib/conjVerbByAPI.ts
@@ -4,7 +4,11 @@ import { ni } from './normalizeVerb';
 export const conjVerbByAPI = async (verb: string) => {
   
   const conjugations = conjugateVerb(ni(verb));
-  await fetch('/api/conjVerb')
-  return conjugations.propOfVerb
+  await fetch('/api/conjVerb', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ verb, conjugations }),
+  });
+  return conjugations.propOfVerb; 
   
 };
